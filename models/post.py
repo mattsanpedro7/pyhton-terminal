aa6715af19700a1a9a5a3286917ae71afd638a46
mattNat <mattsanpedro76@gmail.com>
2018-06-30 01:03:31 -0700

    add model folder and post.py

---
+++ models/post.py
@@ -0,0 +1,8 @@
+class Post(object):
+
+    # which properties the post should have
+    def __init__(self):
+        self.title = "This is my title"
+        self.content = "This is some content"
+        self.author = "Matt"
+
