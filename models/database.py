e5de42fe8809f4ca48c5796f557fddaef9469614
mattNat <mattsanpedro76@gmail.com>
2018-06-30 17:05:40 -0700

    forgot to return for find and find_one static methods

--- models/database.py
+++ models/database.py
@@ -23,10 +23,9 @@ class Database(object):
 
     @staticmethod
     def find(collection, query):
-        Database.DATABASE[collection].find(query)
+        return Database.DATABASE[collection].find(query)
 
     @staticmethod
     def find_one(collection, query):
-        Database.DATABASE[collection].find_one(query)
-
+        return Database.DATABASE[collection].find_one(query)
 
