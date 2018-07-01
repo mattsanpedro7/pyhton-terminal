409caba3845dd0ca1f82fdd48adc8e24b02ea4e8
mattNat <mattsanpedro76@gmail.com>
2018-06-30 17:06:01 -0700

    import and initialize Database class

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # models is container with class Post
 # basis for object oriented programming
 from models.post import Post
+from models.database import Database
+
+# initialize db
+Database.initialize()
 
 # an instance of class
 post = Post('Hello', 'World', 'Matt')
