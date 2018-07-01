be11de205430fbfc8c8207ae75a7a60abc3a6d5b
mattNat <mattsanpedro76@gmail.com>
2018-06-30 17:19:48 -0700

    add two static methods: 1) from_mongo: fetch post by id.  2) fetch posts given a blog id

--- models/post.py
+++ models/post.py
@@ -1,6 +1,7 @@
 # things that have same properties
-class Post(object):
+from models.database import Database
 
+class Post(object):
     # which properties the post should have
     # init: method I am going to create...
     def __init__(self, title, content, author):
@@ -25,3 +26,16 @@ class Post(object):
             'title': self.title,
             'created_date': self.created_date
         }
+
+    # method: post from mongo given an id and it will give us back data
+    @staticmethod
+    def from_mongo(id):
+        # don't have to write "collection="
+        # Post.from_mongo('123')
+        return Database.find_one(collection='posts', query={'id', id})
+
+    @staticmethod
+    def from_blog(id):
+        return [post for post in Database.find(collection='posts', query={'blog_id': id})]
+
+
