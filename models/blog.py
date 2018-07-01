876730f2a241fd4feaab3628580774adb8b44ca1
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-07-01 16:12:48 -0700

    correct type for "hex()"

--- models/blog.py
+++ models/blog.py
@@ -9,7 +9,7 @@ class Blog(object):
         self.author = author
         self.title = title
         self.description = description
-        self.id = uuid.uuid4().hex() if id is None else id
+        self.id = uuid.uuid4().hex if id is None else id
 
     def new_post(self):
         title = input('Enter post title: ')
